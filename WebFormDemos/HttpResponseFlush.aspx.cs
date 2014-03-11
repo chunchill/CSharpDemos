f9b2b220b5100c1232f209da6df533b7182cb76f
chunchill <chunchill@163.com>
2014-03-11 12:38:23 +0800

    add web applications.

---
+++ WebFormDemos/HttpResponseFlush.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace WebFormDemos
+{
+   public partial class HttpResponseFlush : System.Web.UI.Page
+   {
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         // Set the page's content type to JPEG files
+         // and clears all content output from the buffer stream.
+         Response.ContentType = "image/jpeg";
+         Response.Clear();
+
+         // Buffer response so that page is sent
+         // after processing is complete.
+         Response.BufferOutput = true;
+
+         // Create a font style.
+         var rectangleFont = new Font(
+             "Arial", 10, FontStyle.Bold);
+
+         // Create integer variables.
+         const int height = 100;
+         const int width = 200;
+
+         // Create a random number generator and create
+         // variable values based on it.
+         var r = new Random();
+         var x = r.Next(75);
+         int a = r.Next(155);
+         int x1 = r.Next(100);
+
+         // Create a bitmap and use it to create a
+         // Graphics object.
+         var bmp = new Bitmap(
+             width, height, PixelFormat.Format24bppRgb);
+         var g = Graphics.FromImage(bmp);
+
+         g.SmoothingMode = SmoothingMode.AntiAlias;
+         g.Clear(Color.LightGray);
+
+         // Use the Graphics object to draw three rectangles.
+         g.DrawRectangle(Pens.White, 1, 1, width - 3, height - 3);
+         g.DrawRectangle(Pens.Aquamarine, 2, 2, width - 3, height - 3);
+         g.DrawRectangle(Pens.Black, 0, 0, width, height);
+
+         // Use the Graphics object to write a string
+         // on the rectangles.
+         g.DrawString(
+             "ASP.NET Samples", rectangleFont,
+             SystemBrushes.WindowText, new PointF(10, 40));
+
+         // Apply color to two of the rectangles.
+         g.FillRectangle(
+             new SolidBrush(
+                 Color.FromArgb(a, 255, 128, 255)),
+             x, 20, 100, 50);
+
+         g.FillRectangle(
+             new LinearGradientBrush(
+                 new Point(x, 10),
+                 new Point(x1 + 75, 50 + 30),
+                 Color.FromArgb(128, 0, 0, 128),
+                 Color.FromArgb(255, 255, 255, 240)),
+             x1, 50, 75, 30);
+
+         // Save the bitmap to the response stream and
+         // convert it to JPEG format.
+         bmp.Save(Response.OutputStream, ImageFormat.Jpeg);
+
+         // Release memory used by the Graphics object
+         // and the bitmap.
+         g.Dispose();
+         bmp.Dispose();
+
+         // Send the output to the client.
+         Response.Flush();
+      }
+   }
+}
---
+++ WebFormDemos/ResponseFlush2.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormDemos
+{
+   public partial class ResponseFlush2 : System.Web.UI.Page
+   {
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         Response.BufferOutput = true;
+         Response.Write(GetString("1"));
+         Response.Flush();
+         Response.Clear();
+         Thread.Sleep(1000);
+         Response.Write(GetString("2"));
+         Response.Flush();
+         Response.Clear();
+         Thread.Sleep(1000);
+         Response.Write(GetString("3"));
+         Response.Flush();
+         Response.Clear();
+         Thread.Sleep(1000);
+         Response.Write(GetString("4"));
+         Response.Flush();
+         Response.Clear();
+         Thread.Sleep(1000);
+         Response.Write(GetString("5"));
+         Response.Flush();
+         Response.Clear();
+         Thread.Sleep(1000);
+      }
+      private string GetString(string val)
+      {
+         StringBuilder sb = new StringBuilder();
+         for (int i = 0; i < 256; ++i)
+         {
+            sb.Append(val);
+         }
+         sb.Append("<br />");
+         //确保sb里面的字符串大于256字节
+         return sb.ToString();
+      }
+   }
+}
